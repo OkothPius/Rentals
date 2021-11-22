b11bc8eecec8089e4b43fa51ec37677c97790061
OkothPius <orukopius8@gmail.com>
2021-11-22 18:44:41 +0300

    feat: update accounts

--- backend/apps/accounts/apps.py
+++ backend/apps/accounts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.apps.accounts'
+
+    def ready(self):
+        import backend.apps.accounts.signals
