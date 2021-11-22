a8947cb42f9774fe2c7c854ee2f883e3d86c2ad6
OkothPius <orukopius8@gmail.com>
2021-11-22 18:45:03 +0300

    feat: update core app

--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -1,6 +1,36 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    estate = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'{self.estate}'
 
 class Rental(models.Model):
+    name = models.CharField(max_length=100)
+    rent = models.IntegerField()
+    house_detail = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    type = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.name
+
+        # Handles redirect
+    def get_absolute_url(self):
+        return reverse('home')
+
+class Sale(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     house_detail = models.TextField()
@@ -13,4 +43,8 @@ class Rental(models.Model):
         ordering = ['-pub_date']
 
     def __str__(self):
-        return self.title
+        return self.name
+
+        # Handles redirect
+    def get_absolute_url(self):
+        return reverse('home')
