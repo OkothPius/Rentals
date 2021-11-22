f504bce063f5b4cd3b397edacbf432d45dcfd4ec
OkothPius <orukopius8@gmail.com>
2021-11-22 20:25:45 +0300

    feat: update admin

--- backend/apps/core/admin.py
+++ backend/apps/core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Rental
+from .models import Rental, Sale, Category
 
 admin.site.register(Rental)
+admin.site.register(Sale)
+admin.site.register(Category)
