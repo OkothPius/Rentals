fd68830e976f0a12f9601379a73e3567bcd961fd
OkothPius <orukopius8@gmail.com>
2021-11-25 13:33:54 +0300

    feat: urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,16 +1,29 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LoginView, LogoutView
+from accounts import views as user_views
 
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.apps.core.urls')),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('oauth/', include('social_django.urls', namespace='social')),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('password-reset/',
+        auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+        name='password_reset'),
+    path('password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+        name='password_reset_done'),
+    path('password-reset_confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    path('password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+        name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
@@ -19,3 +32,4 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
