bc25242b831acb91553ad14b0b5dbdc67e3ff191
OkothPius <orukopius8@gmail.com>
2021-11-25 16:32:31 +0300

    feat: opertations & func for accounts app

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+# from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pics')
+    # image = models.ImageField(upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} profile'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.path)
+    #
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
 # default='default.jpg',
