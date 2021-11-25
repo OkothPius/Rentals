3453d40458b470cdc0ac8843422727a5e070299d
OkothPius <orukopius8@gmail.com>
2021-11-25 13:33:13 +0300

    feat: update urls

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-from .views import HomeListView
+from .views import HomeListView, RentalCreateView,RentalUpdateView, RentalDeleteView
 
 urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
     path('geodata/', views.get_map, name='map'),
+    path('rental/<int:pk>/update/', RentalUpdateView.as_view(), name='rental-update'),
+    path('rental/<int:pk>/delete/', RentalDeleteView.as_view(), name='rental-delete'),
 ]
