49ea03cb5ba3722404af5200633b2f5f4a3d1f92
OkothPius <orukopius8@gmail.com>
2021-11-25 21:56:59 +0300

    feat: update models

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -4,19 +4,19 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # image = models.ImageField(upload_to='profile_pics')
+    image = models.ImageField(upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} profile'
 
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-    #
-    #     img = Image.open(self.image.path)
-    #
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 # default='default.jpg',
