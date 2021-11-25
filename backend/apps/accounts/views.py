19ad83011cf9bf18f5268b6eba26fc6f491f55df
OkothPius <orukopius8@gmail.com>
2021-11-25 21:57:13 +0300

    feat: update views

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from . forms import UserRegisterForm, UserUpdateForm
+from . forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -21,20 +21,20 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance = request.user)
-        # p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
 
-        if u_form.is_valid():
+        if u_form.is_valid() and p_form.is_valid():
             u_form.save()
-            # p_form.save()
+            p_form.save()
             messages.success(request, f'Your account has been Updated!')
-            return redirect('home')
+            return redirect('profile')
     else:
         u_form = UserUpdateForm(instance = request.user)
-        # p_form = ProfileUpdateForm(instance = request.user.profile)
+        p_form = ProfileUpdateForm(instance = request.user.profile)
 
 
     context = {
         'u_form': u_form,
-        # 'p_form': p_form
+        'p_form': p_form
     }
     return render(request, 'accounts/profile.html', context)
