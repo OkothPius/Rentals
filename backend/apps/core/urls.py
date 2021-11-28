cdf73a58fa86607313e5782414443737d8ae19bb
OkothPius <orukopius8@gmail.com>
2021-11-28 13:16:11 +0300

    feat: update

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -3,8 +3,9 @@ from . import views
 from .views import HomeListView, RentalCreateView,RentalUpdateView, RentalDeleteView, SearchView, PdfView
 
 urlpatterns = [
+    # path('home/', views.home, name='home'),
+    path('map/', views.default_maps, name='map'),
     path('', HomeListView.as_view(), name='home'),
-    path('geodata/', views.get_map, name='map'),
     path('rental/new/', RentalCreateView.as_view(), name='rental-create'),
     path('rental/<int:pk>/update/', RentalUpdateView.as_view(), name='rental-update'),
     path('rental/<int:pk>/delete/', RentalDeleteView.as_view(), name='rental-delete'),
