2e471b2a07d4510c7251b4ed03e1abfef734834c
Okoth Pius <orukopius8@gmail.com>
2021-12-05 14:37:09 +0300

    feat: update routes

--- backend/urls.py
+++ backend/urls.py
@@ -5,12 +5,18 @@ from backend.apps.accounts import views as user_views
 
 from django.conf import settings
 from django.conf.urls.static import static
+# from backend.apps.invoice.views import InvoiceListView, createInvoice, generate_PDF, view_PDF
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.apps.core.urls')),
     path('invoice/', include('backend.apps.invoice.urls')),
+    # path('invoice-list/', InvoiceListView.as_view(), name="invoice-list"),
+    # path('create/', createInvoice, name="invoice-create"),
+    # path('invoice-detail/<id>', view_PDF, name='invoice-detail'),
+    # path('invoice-download/<id>', generate_PDF, name='invoice-download'),
 
 ]
 
