ea3c0eb6d7b4e69fb96dc4cb48e0387cb932071a
Okoth Pius <orukopius8@gmail.com>
2021-12-05 13:21:17 +0300

    feat: uodate

--- backend/urls.py
+++ backend/urls.py
@@ -10,6 +10,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.apps.core.urls')),
+    path('invoice/', include('backend.apps.invoice.urls')),
+
 ]
 
 if settings.DEBUG:
