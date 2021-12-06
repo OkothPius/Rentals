b4271d075dd6de965262f4306f66bc14133eb477
Okoth Pius <orukopius8@gmail.com>
2021-12-06 17:03:19 +0300

    feat: update

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -2,80 +2,119 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 # from . forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-from .forms import SignUpForm, LoginForm
 from django.contrib.auth import authenticate, login
+from django.views.generic import CreateView
+
+from .models import User, Tenant
+from .forms import TenantSignUpForm, AgentSignUpForm, UserUpdateForm
+
+
 
 def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username} Log In to Proceed!')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    context = {'form':form}
-    return render(request, 'accounts/register.html', context)
+    return render(request, 'accounts/register.html')
 
 
+class TenantSignUpView(CreateView):
+    model = User
+    form_class = TenantSignUpForm
+    template_name = 'accounts/signup.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'tenant'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('rental')
+
+
+class AgentSignUpView(CreateView):
+    model = User
+    form_class = AgentSignUpForm
+    template_name = 'accounts/signup.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'agent'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        # login(self.request, user)
+        return redirect('rental')
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Account created for {username} Log In to Proceed!')
+#             return redirect('login')
+#     else:
+#         form = UserRegisterForm()
+#     context = {'form':form}
+#     return render(request, 'accounts/register.html', context)
+#
+#
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance = request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
+        # p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
 
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
-            p_form.save()
+            # p_form.save()
             messages.success(request, f'Your account has been Updated!')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance = request.user)
-        p_form = ProfileUpdateForm(instance = request.user.profile)
+        # p_form = ProfileUpdateForm(instance = request.user.profile)
 
 
     context = {
         'u_form': u_form,
-        'p_form': p_form
+        # 'p_form': p_form
     }
     return render(request, 'accounts/profile.html', context)
 
 def index(request):
     return render(request, 'accounts/index.html')
-
-
-def register(request):
-    msg = None
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            msg = 'user created'
-            return redirect('login_view')
-        else:
-            msg = 'form is not valid'
-    else:
-        form = SignUpForm()
-    return render(request,'accounts/register.html', {'form': form, 'msg': msg})
-
-
-def login_view(request):
-    form = LoginForm(request.POST or None)
-    msg = None
-    if request.method == 'POST':
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None and user.is_agent:
-                login(request, user)
-                return redirect('home')
-            elif user is not None and user.is_user:
-                login(request, user)
-                return redirect('home')
-            else:
-                msg= 'invalid credentials'
-        else:
-            msg = 'error validating form'
-    return render(request, 'accounts/login.html', {'form': form, 'msg': msg})
+#
+#
+# def register(request):
+#     msg = None
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             msg = 'user created'
+#             return redirect('login_view')
+#         else:
+#             msg = 'form is not valid'
+#     else:
+#         form = SignUpForm()
+#     return render(request,'accounts/register.html', {'form': form, 'msg': msg})
+#
+#
+# def login_view(request):
+#     form = LoginForm(request.POST or None)
+#     msg = None
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(username=username, password=password)
+#             if user is not None and user.is_agent:
+#                 login(request, user)
+#                 return redirect('home')
+#             elif user is not None and user.is_user:
+#                 login(request, user)
+#                 return redirect('home')
+#             else:
+#                 msg= 'invalid credentials'
+#         else:
+#             msg = 'error validating form'
+#     return render(request, 'accounts/login.html', {'form': form, 'msg': msg})
