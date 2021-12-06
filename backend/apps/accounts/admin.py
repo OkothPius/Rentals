94fcf536f8bd7972e459a42586c0bfd4ccaedd7f
Okoth Pius <orukopius8@gmail.com>
2021-12-06 17:01:53 +0300

    feat: update models

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Tenant, Agent
 
 admin.site.register(User)
+admin.site.register(Tenant)
+admin.site.register(Agent)
