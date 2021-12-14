345b2e2adbc103bb682df1cbcc27f54c58e5eac1
Okoth Pius <orukopius8@gmail.com>
2021-12-14 14:26:32 +0300

    feat: update

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -4,10 +4,15 @@ from .views import register,profile, TenantSignUpView, AgentSignUpView
 
 
 urlpatterns = [
-    path('signup/',register, name='register'),
-    path('profile/',profile, name='profile'),
-    path('signup/tenant/', TenantSignUpView.as_view(), name='tenant_signup'),
-    path('signup/agent/', AgentSignUpView.as_view(), name='agent_signup'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+
+    path('login_form/', views.login_form, name='login_form'),
+    path('login/', views.loginView, name='login'),
+    path('logout/', views.logoutView, name='logout'),
+
+    # path('signup/',register, name='register'),
+    # path('profile/',profile, name='profile'),
+    # path('signup/tenant/', TenantSignUpView.as_view(), name='tenant_signup'),
+    # path('signup/agent/', AgentSignUpView.as_view(), name='agent_signup'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 
 ]
