8ff42241628f857b07c4960c554a305b099a37e5
Okoth Pius <orukopius8@gmail.com>
2021-12-14 14:27:12 +0300

    feat: update

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -18,20 +18,38 @@ from .views import (
             )
 
 urlpatterns = [
-    # path('home/', views.home, name='home'),
-    path('map/', views.default_maps, name='map'),
+
+    #Authentication
+
+    #Agent URLS 
+
+    #Tenant URLS
+
+    #Rental CRUD operations
     path('', RentalListView.as_view(), name='rental'),
     path('rental/new/', RentalCreateView.as_view(), name='rental-create'),
     path('rental/<int:pk>/update/', RentalUpdateView.as_view(), name='rental-update'),
     path('rental/<int:pk>/delete/', RentalDeleteView.as_view(), name='rental-delete'),
-    path('sale/new/', SaleCreateView.as_view(), name='sale-create'),
+
+    #Sale CRUD operations
     path('sale/', SaleListView.as_view(), name='sale'),
+    path('sale/new/', SaleCreateView.as_view(), name='sale-create'),
     path('sale/<int:pk>/update/', SaleUpdateView.as_view(), name='sale-update'),
     path('sale/<int:pk>/delete/', SaleDeleteView.as_view(), name='sale-delete'),
+
+    #Search
     path('search/', SearchView.as_view(), name='search'),
+
+    #Map
+    path('map/', views.default_maps, name='map'),
+
+    #Report Download
     path('rental-detail/', view_PDF, name='rental-detail'),
     path('rental-download/', generate_PDF, name='rental-download'),
     path('download/', PdfView.as_view(), name='download'),
     path('sale-download/', sale_generate_PDF, name='sale-download'),
     path('sale-detail/', sale_PDF, name='sale-detail'),
+
+
+
 ]
