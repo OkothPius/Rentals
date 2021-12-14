a30f739a298b80ce7e0e03e720d04255aa7c0fe5
Okoth Pius <orukopius8@gmail.com>
2021-12-14 16:00:45 +0300

    feat: update pub_date

--- backend/apps/core/admin.py
+++ backend/apps/core/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from .models import Rental, Sale, Category
 
 
-class RentalAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,                         {'fields': ['pub_date', 'name']}),
-        ('House Description Details' , {'fields': ['house_detail', 'rent', 'location', 'image'], 'classes': ['collapse']}),
-    ]
+# class RentalAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,                         {'fields': ['name']}),
+#         ('House Description Details' , {'fields': ['house_detail', 'rent', 'location', 'image'], 'classes': ['collapse']}),
+#     ]
+#
+#     list_display = ('name')
+#
+#     # list_filter = ['pub_date']
+#
+#     search_fields = ['name']
 
-    list_display = ('name', 'pub_date')
-
-    list_filter = ['pub_date']
-
-    search_fields = ['name']
-
-admin.site.register(Rental, RentalAdmin)
+admin.site.register(Rental)
 admin.site.register(Sale)
 admin.site.register(Category)
